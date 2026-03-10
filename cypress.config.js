ba5ed7c49fd06b3696b4efa5a1b18296855b32a7
Vadym Tymeichuk <40869955+Vadym11@users.noreply.github.com>
2026-03-10 12:08:29 +0000

    change user agent

--- cypress.config.js
+++ cypress.config.js
@@ -14,13 +14,17 @@ module.exports = defineConfig({
     userPassword: process.env.USER_PASSWORD,
   },
   e2e: {
-    // this is needed to avoid 403 Forbidden errors
-    headers: {
-    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36'
-    },
     baseUrl: BASE_URL,
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      // this is needed to avoid 403 Forbidden errors when running tests in Chrome
+      on('before:browser:launch', (browser, launchOptions) => {
+        if (browser.name === 'chrome') {
+          launchOptions.args.push(
+            '--user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 Chrome/120.0.0.0 Safari/537.36'
+          );
+        }
+        return launchOptions;
+      });
     },
     specPattern: 'cypress/e2e/**/*.{cy,spec}.{js,jsx,ts,tsx}'
   },
