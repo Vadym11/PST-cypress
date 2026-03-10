7a9cf6bf6f762bea4009d9b852a6c0eadc4b44cd
Vadym Tymeichuk <40869955+Vadym11@users.noreply.github.com>
2026-03-10 14:54:17 +0000

    debug 3

--- cypress/e2e/api/api-test.spec.ts
+++ cypress/e2e/api/api-test.spec.ts
@@ -1,7 +1,7 @@
 import { loadAdminCreds, urls } from "../../support/utils/project-utils"
 import { generateRandomUserData } from "../../support/utils/test-utils";
 
-describe('API tests', () => {
+describe.skip('API tests', () => {
 
     let email: string;
     let password: string;
