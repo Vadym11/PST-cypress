3a8b2d03a45ec1433d58fec915e8e5ade1f66c04
Vadym Tymeichuk <40869955+Vadym11@users.noreply.github.com>
2026-05-09 17:22:52 +0100

    cart tests added (#10) [skip ci]

--- cypress/support/page-objects/Cart/CartMainPage.ts
+++ cypress/support/page-objects/Cart/CartMainPage.ts
@@ -4,6 +4,10 @@ import { CartSignInPage } from "./CartSignInPage";
 export class CartMainPage extends CartBasePage {
 
     private readonly proceedToCheckoutButtonTestId = 'proceed-1';
+    private readonly productTitleTestId = 'product-title';
+    private readonly productQuantityTestId = 'product-quantity';
+    private readonly deleteProductButtonTestId = 'delete-product';
+    private readonly cartTotalTestId = 'cart-total';
 
     clickProceedToCheckout() {
         // using force click since the cart icon is covered by the pop-up when a product
@@ -11,5 +15,31 @@ export class CartMainPage extends CartBasePage {
         cy.findByTestId(this.proceedToCheckoutButtonTestId).click({force: true});
 
         return new CartSignInPage();
+    }
+
+    getProductTitles(): Cypress.Chainable<JQuery<HTMLElement>> {
+        return cy.findAllByTestId(this.productTitleTestId);
+    }
+
+    getProductQuantityInputByIndex(index: number): Cypress.Chainable<JQuery<HTMLElement>> {
+        return cy.findAllByTestId(this.productQuantityTestId).eq(index);
+    }
+
+    updateProductQuantityByIndex(index: number, newQuantity: number): CartMainPage {
+        cy.findAllByTestId(this.productQuantityTestId).eq(index).focus().type('{selectall}' + String(newQuantity)).blur();
+        return this;
+    }
+
+    removeProductByIndex(index: number): CartMainPage {
+        cy.get('.btn.btn-danger').eq(index).click();
+        return this;
+    }
+
+    getCartTotal(): Cypress.Chainable<JQuery<HTMLElement>> {
+        return cy.findByTestId(this.cartTotalTestId);
+    }
+
+    getQuantityErrorMessage(): Cypress.Chainable<JQuery<HTMLElement>> {
+        return cy.findByRole('alert');
     }
 }
